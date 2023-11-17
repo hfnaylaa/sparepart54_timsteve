778b2248d1e8c9e6566a8db069ce9b0995ead87c
Hikmafitriani <hikmafitri93@gmail.com>
2023-11-17 09:11:47 +0700

    penambahan adapter

--- app/src/main/java/com/iffa/sparepart54_timsteve/recycle_vw.kt
+++ app/src/main/java/com/iffa/sparepart54_timsteve/recycle_vw.kt
@@ -2,10 +2,18 @@ package com.iffa.sparepart54_timsteve
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iffa.sparepart54_timsteve.databinding.ActivityRecycleVwBinding
+import com.iffa.sparepart54_timsteve.roomsteve.Sparepart54
 
 class recycle_vw : AppCompatActivity() {
+
+    private lateinit var findbind: ActivityRecycleVwBinding
+    private lateinit var adapter1: Sparepart54
+    private val db by lazy { Sparepart54(this,) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recycle_vw)
+        findbind = ActivityRecycleVwBinding.inflate(layoutInflater)
+        setContentView(findbind.root)
     }
 }
